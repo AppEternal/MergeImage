98c460417b2a370a8ff38c33d7a530dc2e54e663
AppEternal <44738706+AppEternal@users.noreply.github.com>
2020-08-01 15:10:20 -0500

    Add files via upload
    Added average option and auto scaling to height.

--- index.js
+++ index.js
@@ -5,24 +5,52 @@ const readdir = promisify(require('fs').readdir);
 const path = require('path');
 
 const init = async () => {
-let settings = './settings.json'
-let config = require(settings);
+let config = require('./settings.json');
 
 let outWidth = config.outputWidth;
 let outHeight = config.outputHeight;
 let inputDir = config.inputDir;
 let outputDir = config.outputDir;
+let useAverage = config.useAverage;
 
 const imgFiles = await readdir(path.join(__dirname, inputDir));
 
+const loaded = []
+let avg_width = 0;
+let avg_height = 0;
+
+for (const key in imgFiles) {
+    let img = await loadImage(path.join(__dirname, inputDir, imgFiles[key]))
+    loaded.push(img)
+    avg_width += img.width
+    avg_height += img.height
+}
+avg_width /= imgFiles.length
+avg_height /= imgFiles.length
+
+if(useAverage){
+    outWidth = avg_width*2
+    outHeight = avg_height
+}
+
 let cnt = 0;
-for (let i = 0; i < imgFiles.length; i+=2) {
+for (let i = 0; i < loaded.length; i+=2) {
     const canvas = createCanvas(outWidth, outHeight)
     const ctx = canvas.getContext('2d')
-    let img_1 = await loadImage(path.join(__dirname, inputDir,imgFiles[i]))
-    let img_2 = await loadImage(path.join(__dirname, inputDir,imgFiles[i+1]))
-    ctx.drawImage(img_1, 0, 0, img_1.width, img_1.height);
-    ctx.drawImage(img_2, (outWidth-img_2.width), 0, img_2.width, img_2.height);
+    let img_1 = loaded[i]
+    let img_2 = loaded[i+1]
+
+    let adg_1 = (outHeight-img_1.height)
+    let img_1_h = img_1.height+adg_1;
+    let img_1_w = (adg_1>0)?img_1.width+adg_1:img_1.width+adg_1/2;
+    
+    let adg_2 = (outHeight-img_2.height)
+    let img_2_h = img_2.height+(outHeight-img_2.height);
+    let img_2_w = (adg_2>0)?img_2.width+adg_2:img_2.width+adg_2/2;
+    
+
+    ctx.drawImage(img_1, 0, 0, img_1_w, img_1_h);
+    ctx.drawImage(img_2, (outWidth-img_2_w), 0, img_2_w, img_2_h);
 
     let out = fs.createWriteStream(path.join(__dirname, outputDir,`${cnt}.jpg`))
     let stream = canvas.createJPEGStream({ quality: 1.0 , chromaSubsampling: true, progressive: true})
